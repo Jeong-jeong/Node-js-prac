b5570bfc18aa6813065523ef7e40dbc0f67df94b
Jeong-jeong <ghkdvnfld345@naver.com>
2021-04-30 14:37:17 +0900

    📝  Learn middleware

---
+++ expressReview/app3.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+	console.log('첫번째 미들웨어 호출됨.');
+
+	req.user = 'mike';
+
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log('두번째 미들웨어 호출됨.');
+
+	res.writeHead(200, {'Content-Type': 'text/html;charset=utf8'});
+	res.end(`<h1>server에서 응답한 결과임. : ${req.user}</h1>`);
+})
+
+const server = http.createServer(app).listen(app.get('port'), () => {
+	console.log(`express로 웹 서버 실행함 ${app.get('port')}`)
+})
