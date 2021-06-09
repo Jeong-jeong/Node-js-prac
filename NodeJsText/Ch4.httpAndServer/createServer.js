1657ada313d3d2d09f420842c1fd75b10510a5ea
Jeong-jeong <ghkdvnfld345@naver.com>
2021-06-09 16:07:50 +0900

    📝  server listen with listening event listener

--- NodeJsText/Ch4.httpAndServer/createServer.js
+++ NodeJsText/Ch4.httpAndServer/createServer.js
@@ -1,13 +1,19 @@
 const http = require('http');
 
-http.createServer((req, res) => { // 요청에 대한 콜백함수
+const server = http.createServer((req, res) => { // 요청에 대한 콜백함수
 	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 	res.write('<h1>Hello Node!</h1>');
 	res.end('<p>Hello Server!</p>');
 })
-	.listen(8080, () => { // 서버 연결
-		console.log('8080번 포트에서 서버 대기중입니다!');
-	});
+server.listen(8080);
+
+// listening 이벤트 리스너
+server.on('listening', () => {
+	console.log('8080번 포트에서 대기중!');
+});
+server.on('error', (err) => {
+	console.error(err);
+});
 
 
 	
